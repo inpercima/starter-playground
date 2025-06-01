70e5cc66daaf6e1dd4d95fecfb8c7854dcc88c20
Marcel Hendrich <inpercima@gmail.com>
2025-06-01 12:06:26 +0200

    chore(): update @angular to 20.0.0

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class AppComponent {
-  title = 'angular';
+export class App {
+  protected title = 'angular';
 }
